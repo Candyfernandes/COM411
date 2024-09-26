e51c023dcca9bf281380321e43d0ab1caa6e0c5e
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-09-26 15:51:45 +0100

    finished.

--- week1/input_task.pay.py
+++ week1/input_task.pay.py
@@ -1,3 +1,12 @@
 print("What is your name?")
 name = input()
 print(f"It is nice to meet you {name}")
+print ("how old are you (in years)")
+years = int(input())
+print ("how tall are you (in meters)")
+meters = float(input())
+print ("how much do you weight (in kilograms)")
+kilograms = float(input())
+
+bmi = kilograms / (meters ** 2 )
+print(" name yor are years years old and your BMI is")
