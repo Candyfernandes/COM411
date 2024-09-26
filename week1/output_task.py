aa45d65bc1c3b28569dcbc48e09180e8acfdf173
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-09-26 15:02:24 +0100

    testing PyCharm with GitHub

--- week1/output_task.py
+++ week1/output_task.py
@@ -1,1 +1,6 @@
 print("welcome to COM411!")
+print ("In week 1 we will learn ....")
+print("... how to use Git and GitHub")
+print("... How to output to the screen")
+print ( "... How to get user input " )
+print ("I hope you enjoying the lesson thus far ")
