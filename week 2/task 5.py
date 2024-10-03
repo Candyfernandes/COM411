994039866792bdc9c9426782954247e554d3b2e5
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-10-03 14:22:39 +0100

    greater then or less then

---
+++ week 2/task 5.py
@@ -0,0 +1,14 @@
+print("Please enter the first_number.")
+first_number = int(input())
+print("Please enter the second_number.")
+second_number =  int(input())
+
+
+if first_number > second_number :
+    print("first number is greater than second number")
+elif first_number == second_number :
+    print("first number is equal to second number")
+elif first_number < second_number :
+    print("second number is greater than first number")
+
+
