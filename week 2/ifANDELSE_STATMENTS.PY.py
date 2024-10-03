91c04a08f6e62a9b91f2adcc34bd72030cd36762
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-10-03 13:31:51 +0100

    added code to demonstarte the use of an if and else statement

---
+++ week 2/ifANDELSE_STATMENTS.PY.py
@@ -0,0 +1,9 @@
+print("please entry the activity to be performed.")
+activity = input()
+
+if activity =="calculate":
+    print("Performing calculations...")
+elif activity == "Running":
+    print("Performing activity..")
+
+print("Activity completed.")
