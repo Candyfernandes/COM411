1f2e633c05de17a6dbf6b6ce258bda9a60a61cef
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-10-03 14:38:36 +0100

    odd and even

---
+++ week 2/task 6.py
@@ -0,0 +1,26 @@
+even_count = 0
+odd_count = 0
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    even_count += 1
+else:
+    odd_count += 1
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    even_count += 1
+else:
+    odd_count += 1
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    even_count += 1
+else:
+    odd_count += 1
+
+print (even_count, odd_count)
+
