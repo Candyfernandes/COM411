a366118a7887e8640b10f9c69d3f64d65ca153b2
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-10-17 14:47:28 +0100

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ week 4/task 4.py
@@ -0,0 +1,12 @@
+def identify():
+
+    print("what lies before us?")
+    before = input()
+
+    if before == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+
+identify()
