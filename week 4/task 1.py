944423200b951cc29d8a0077e2fcb303678d8a83
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-10-17 13:16:31 +0100

    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ week 4/task 1.py
@@ -0,0 +1,13 @@
+def main ():
+    print ("program started!")
+    print("Please enter a standard character:")
+
+user_input = input("Enter a character: ")
+
+if len(user_input) == 1:
+    ascii_value = ord(user_input)
+    print(f"The ASCII code for {user_input} is {ascii_value}.")
+else:
+    print("Error: Please enter a single character.")
+
+print("Program Ended!")
