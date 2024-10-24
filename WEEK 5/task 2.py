c5474ef70d31314f8339a0cfee69c554a45e028b
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-10-24 13:30:16 +0100

    task 2

--- WEEK 5/task 2.py
+++ WEEK 5/task 2.py
@@ -1,2 +1,18 @@
-def movements :
+def movements():
     path = ["Move forward",10, "Move backwards" , 5, "move Left", 3 , "move Right" , "move Up" , 1]
+    return path
+
+
+def run_task2():
+    print("Moving....")
+    path = movements()
+    for i in range(0, len(path), 2):
+        direction = path[i]
+        steps = path[i + 1]
+        print(f"{direction} for {steps} steps")
+
+
+if __name__ == "__main__":
+    run_task2()
+
+
