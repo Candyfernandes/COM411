a08dbad9be32680964b68a735453d41de1a5824c
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-10-24 13:20:37 +0100

    exercise 1

---
+++ WEEK 5/Task 1.py
@@ -0,0 +1,11 @@
+def directions():
+      steps = ["Move forward", "moving backwards", "turn left", "turn right"]
+      return steps
+
+def run_task1():
+     steps = directions()
+     print(steps)
+
+
+if __name__ == "__main__":
+    run_task1()
---
+++ WEEK 5/task 2.py
@@ -0,0 +1,2 @@
+def movements :
+    path = ["Move forward",10, "Move backwards" , 5, "move Left", 3 , "move Right" , "move Up" , 1]
