4e04d1ee9d1748f3c651a8ea34f9ffe6bcfeff57
EULOGIA FERNANDES <0ferne79@solent.ac.uk>
2024-11-07 14:01:10 +0000

    exercise 1

--- week 7/Task 1.py
+++ week 7/Task 1.py
@@ -1,4 +1,14 @@
-observation = set()
-observation = {"Car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"}
-print (observation)
+def observed ():
+    observation = {"Car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"}
+    return observation
+
+
+def run_task1 ():
+    observation = observed()
+    print(observation)
+
+run_task1()
+
+
+
 
